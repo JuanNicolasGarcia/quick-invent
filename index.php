5066db2de575af8fd54651494d94797ddde6cecd
Nick Garcia <juan.garcia07@utrgv.edu>
2017-09-27 18:34:28 -0500

    changed index from html to php

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>QuickManage Inventory!</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="styles.css" rel="stylesheet">
+    <link href="jumbotron.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <!-- Nav bar code -->
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">QuickManage</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sign Up/Log In</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="https://google.com">Log In</a>
+              <a class="dropdown-item" href="https://google.com">Sign Up!</a>
+              <a class="dropdown-item" href="https://google.com">Help?</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+    <!-- Hook element -->
+
+    <div class="container">
+      <div class="starter-template">
+        <h1>Find, Catalog, Manage, Easily!</h1>
+       <h4>Management of your business inventory can always be a daunting task.  Who has what?  Where is that?  How many of this?  We have the solution for you.</h4>
+      </div>
+
+    </div>
+
+    <!-- product intro element -->
+
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-4 text-center">QuickManage Inventory Service!</h1>
+        <h4 class="text-center">Manage your business inventory with just a few clicks!</h4>
+        <div class="text-center"><a class="btn btn-outline-success btn-lg" href="https://google.com" role="button">Start Your Free Trial »</a></div>
+      </div>
+    </div>
+
+    <!-- Three headings code -->
+
+    <div class="container">
+
+      <div class="row">
+        <div class="col-lg-4 text-center">
+          <img src="mag_glass_2.png" alt="Generic placeholder image" width="140" height="140">
+          <h2>Find</h2>
+          <h5>Quickly search your inventory using various search and sorting options depending on what you need.  You can also design you own custom search options.</h5>
+          <p><a class="btn btn-outline-info" href="https://google.com" role="button">View details »</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4 text-center">
+          <img src="tracking_number-512.png" width="140" height="140">
+          <h2>Track</h2>
+          <h5>Keep track of who has what, with easy to use tracking log tools, that help you keep track of your inventory when it moves.</h5>
+          <p><a class="btn btn-outline-info" href="#" role="button">View details »</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4 text-center">
+          <img src="manage.png" width="140" height="140">
+          <h2>Manage</h2>
+          <h5>Transfer, delete, and add items, to your inventory with no paperwork hassle.  Updating inventory item information is also easy with history log tools.</h5>
+          <p><a class="btn btn-outline-info" href="#" role="button">View details »</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div>
+
+      <hr class="featurette-divider" style="margin-top: 30px; margin-bottom: 30px;">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Large Business Inventory? <span class="text-muted">We have the tools to keep you organized.</span></h2>
+          <p class="lead">Searching, updating, moving, removing, and transferring items on your inventory is made simple.  No mountains of paper work are needed. Just a few clicks can help you complete, what normally takes days, in minutes.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="450x400" style="width: 450px; height: 400px;" src="Big-Business-.jpg" data-holder-rendered="true">
+        </div>
+      </div>
+
+      <hr class="featurette-divider" style="margin-top: 30px; margin-bottom: 30px;">
+
+      <div class="row featurette">
+        <div class="col-md-7 order-md-2">
+          <h2 class="featurette-heading">Managing School Inventory? <span class="text-muted">Our tools allow you to stay on track.</span></h2>
+          <p class="lead">Whether it's K-12 or higher education, we can help you track computers, classroom equipment, books, or anything else you need. Our logging tools can also help you manage the checkout/rental of your equipment.</p>
+        </div>
+        <div class="col-md-5 order-md-1">
+          <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="450x400" style="width: 450px; height: 400px;" src="school_it.jpg" data-holder-rendered="true">
+        </div>
+      </div>
+
+      <hr class="featurette-divider" style="margin-top: 25px; margin-bottom: 25px;">
+
+    </div>
+
+        <!-- product intro element -->
+
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-4 text-center">QuickManage Inventory Service!</h1>
+        <h4 class="text-center">Manage your business inventory with just a few clicks!</h4>
+        <h4 class="text-center">At an introductry rate of $39.99 a month for the first 6 months.</h4>
+        <div class="text-center"><a class="btn btn-outline-success btn-lg" href="https://google.com" role="button">Sign Up Now »</a></div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="assets/js/vendor/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
